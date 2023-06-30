96bd19d1a72d03380c38fa48763db283b72c8d41
Josue Williams <josuew913@gmail.com>
2023-06-29 23:16:45 -0400

    movement operational

--- js/script.js
+++ js/script.js
@@ -12,22 +12,23 @@ window.onload = function () {
       game.start()
       console.log("start game");
 
-      console.log(game.gamePage.style.left)
       game.weight.element.onmousemove = ((e) => {
-      if(game.weight.element.style.left === '215px') {
-        game.weight.element.style.left = '214px';
-        game.gamePage.style.cursor = "none";
-        return;
-      }
+        
+        if(game.weight.element.style.left === '206px') {
+          game.weight.element.style.left = 205 + 'px';
+          game.gamePage.style.cursor = "none";
+          return;
+        }
+        
+      
         let mousePosition = e.target.getBoundingClientRect();
         console.log(e.clientX)
-        game.weight.element.style.left = e.clientX - 600 + 'px';
+        game.weight.element.style.left = e.clientX - 700 + 'px';
 
         game.gamePage.style.cursor = "none";
 
         
-
-        console.log(game.weight.element.style.left);
+            console.log(game.weight.element.style.left);
       
 
     })
@@ -51,8 +52,6 @@ window.onload = function () {
             console.log(count);
           }, 1000);
           
-
-          
         };
     
         game.gamePage.onmouseup = (e) => {
@@ -61,52 +60,31 @@ window.onload = function () {
           console.log(intervalId);
           console.log("stopped");
           
-
-          // if(count < 1) {
-          //   game.weight.element.style.top = 250 + 'px';
-          //   score.push(0);
-          
-          // } else if(count > 1 && count < 2) {
-          //   game.weight.element.style.top = 100 + 'px';
-          //   score.push(1);
-          // } else if(count > 2 && count < 3) {
-          //   game.weight.element.style.top = 100 + 'px';
-          //   score.push(2);
-          // } else if(count > 3 && count < 4) {
-          //   game.weight.element.style.top = 100 + 'px';
-          //   score.push(3);
-          // } else if(count > 4) {
-          //   game.weight.element.style.top = 0 + 'px';
-          //   score.push(4);
-          // }
-          // console.log(score)
-          // setTimeout(() => {
-          //   game.weight.element.style.top = 850 + "px";
-          //   count = 0;
-
-        
-          // }, 2000)
-
           if (count < 1) {
             game.weight.element.style.top = 250 + "px";
             score.push(0);
             currentScore.innerText = Number(currentScore.innerText) + 0;
-          } else if (count === 1) {
-            game.weight.element.style.top = 100 + "px";
+          } else if (count === 2) {
+            game.weight.element.style.top = 165 + "px";
             score.push(1);
             currentScore.innerText = Number(currentScore.innerText) + 1;
-          } else if (count === 2) {
+          } else if (count === 3) {
             game.weight.element.style.top = 100 + "px";
             score.push(2);
             currentScore.innerText = Number(currentScore.innerText) + 2;
-          } else if (count === 3) {
-            game.weight.element.style.top = 100 + "px";
-            score.push(3);
-            currentScore.innerText = Number(currentScore.innerText) + 3;
           } else if (count === 4) {
             game.weight.element.style.top = 0 + "px";
+            score.push(3);
+            currentScore.innerText = Number(currentScore.innerText) + 3;
+          } else if (count > 4) {
+            game.weight.element.style.top = -75 + "px";
             score.push(4);
-            currentScore.innerText = Number(currentScore.innerText) + 4;
+            currentScore.innerText = Number(currentScore.innerText) + 0;
+
+            setTimeout(() => {
+              alert("OUT OF BOUNDS!");
+            }, 1000);
+            
           }
 
           tostones--;
@@ -131,15 +109,15 @@ window.onload = function () {
           
         };
 
-        restartButton.addEventListener("click", function () {
-          // Call the restartGame function when the button is clicked
-          restartGame();
-        });
+        // restartButton.addEventListener("click", function () {
+        //   // Call the restartGame function when the button is clicked
+        //   restartGame();
+        // });
       
-        // The function that reloads the page to start a new game
-        function restartGame() {
-          location.reload();
-        }
+        // // The function that reloads the page to start a new game
+        // function restartGame() {
+        //   location.reload();
+        // }
 
 }
   
