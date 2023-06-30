909994a66b1fbf3bab4f04a2d8595632ac03aa70
Josue Williams <josuew913@gmail.com>
2023-06-30 13:00:12 -0400

    game is presentation ready

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ constructor() {
     this.startPage = document.getElementById("start-page");
     this.gamePage = document.getElementById("game-page");
     this.gameEndScreen = document.getElementById("game-end");
+    this.stats = document.querySelector(".stats");
+    
     this.weight = new Weight (this.gamePage, 0, 850, 120, 120, "./images/Toston.png");
     this.height = 97;
     this.width = 300;
@@ -19,7 +21,8 @@ start() {
     this.startPage.style.display = "none";
     this.startPage.style.height = "0px";
     this.gamePage.style.display = "inherit";
-    this.gamePage.style.position = "relative"
+    this.gamePage.style.position = "relative";
+    this.stats.style.display = "flex";
     this.gameLoop()
 
 };
